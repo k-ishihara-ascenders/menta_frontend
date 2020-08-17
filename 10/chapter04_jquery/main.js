3cf301cad0c323af94c14b24519310b12a45bd84
ki5196 <mail@ascenders.co.jp>
2020-08-17 20:11:59 +0900

    税計算アプリ作成

---
+++ 10/chapter04_jquery/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+$(function() {
+	const taxBtn = $('#tax-btn');
+	const taxAmountText = $('#tax-amount');
+	const taxIncludedText = $('#tax-included');
+
+	$(taxBtn).click(function() {
+		const price = $('#price').val() * 1;
+		const taxRate = $('#tax-rate').val() * 1;
+		const taxAmount = Math.floor(price * taxRate / 100);
+		const taxIncluded = price + taxAmount;
+		taxAmountText.text(taxAmount.toString(10));
+
+		console.log(taxAmountText);
+
+		taxIncludedText.text(taxIncluded.toString(10));
+	});
+});

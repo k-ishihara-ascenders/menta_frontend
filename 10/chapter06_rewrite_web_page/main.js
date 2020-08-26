9ac68c4a3faf091cb43bef78b43c12eeb459159c
ki5196 <mail@ascenders.co.jp>
2020-08-26 19:11:23 +0900

    アウトラインのアプリ作成

--- 10/chapter06_rewrite_web_page/main.js
+++ 10/chapter06_rewrite_web_page/main.js
@@ -1,2 +1,23 @@
 'use strict';
 
+$(function() {
+	const tmplt = $('#template').html();
+
+	// 追加
+	$('#btnAppend').click(function() {
+		const text = $('#line').val();
+		const html = tmplt.replace('%s', text);
+		const $target = $('input[name=sel]:checked').parents('li').eq(0);
+		$target.after(html);
+		$target.next().find('input').prop('checked', true);
+	});
+
+	// 入れ子
+	$('#btnNest').click(function() {
+		const text = $('#line').val();
+		const html = '<ul>' + tmplt.replace('%s', text) + '</ul>';
+		const $target = $('input[name=sel]:checked').parents('li').eq(0);
+		$target.append(html);
+		$target.children().find('input').prop('checked', true);
+	});
+});

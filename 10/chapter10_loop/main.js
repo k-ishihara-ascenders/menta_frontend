3f5070c79b8cd4582713c007a3be1783108fba5a
ki5196 <mail@ascenders.co.jp>
2020-09-01 19:52:10 +0900

    連続計算アプリ作成

--- 10/chapter10_loop/main.js
+++ 10/chapter10_loop/main.js
@@ -1,6 +1,28 @@
 'use strict';
 
 $(function() {
+	const calc = () => {
+		const text = $('#source').val();
+		const textArr = text.split('\n');
 
+		const resArr = [];
+		for(let i = 0; i < textArr.length; i++ ) {
+			const src = textArr[i];
+			let res = '';
+			if(src == "") continue;
+
+			try{
+				res = eval(src);
+			} catch(e){
+				res = e;
+			}
+			resArr.push(src + '=' + res);
+
+			const resText = resArr.join('\n');
+			$('#result').val(resText);
+		}
+	}
+
+	$('#btnCalc').click(calc);
 });
 

dd30fddaaf7984248583e945e596d5b46fc5caf6
ki5196 <mail@ascenders.co.jp>
2020-10-16 20:15:53 +0900

    todoタスクに編集機能追加

--- 29/js/main.js
+++ 29/js/main.js
@@ -17,55 +17,23 @@ const vm = new Vue({
 		}, // dateオブジェクト
 		status: '未着手', // デフォルトのステータス
 		isDone: false, // 完了かどうか
-		// isEdit: false, // 編集フォームの表示・非表示
 		todos: [], // タスクの配列
-		// editIndex: 0,
-		// editTodo: '',
-		// editLimit: {
-		// 	year: '',
-		// 	month: '',
-		// 	date: ''
-		// },
-		// editStatus: '',
-		// nowYear: 0, // 期限の年の最低年用
-		// maxYear: 0, // 期限の年の最高年用
+
+		isEdit: false, // 編集用の表示・非表示
+		editIndex: 0,// 編集用のインデックス
+		editTodo: '', // 編集用のタスク名
+		editLimit: { // 編集用の期限日
+			year: '',
+			month: '',
+			date: ''
+		},
 	},
-
-	// computed: {
-	// 	dayMax: function() {
-	// 		const monthList = {
-	// 			day31: [1,3,5,7,8,10,12,'1','3','5','7','8','10','12'],
-	// 			day30: [4,6,9,11,'4','6','9','11'],
-	// 			day29: [2,'2']
-	// 		};
-
-	// 		if(monthList.day31.includes(this.newLimit.month)) {
-	// 			return 31;
-	// 		} else if(monthList.day30.includes(this.newLimit.month)) {
-	// 			return 30;
-	// 		} else if(monthList.day29.includes(this.newLimit.month)) {
-	// 			return 29;
-	// 		}
-	// 	}
-	// },
 
 	methods: {
 		addTodo: function(e) {
-			// const valYear = this.newLimit.year;
-			// const minYear = document.querySelector('input[name=year]').getAttribute('min');
-			// const maxYear = document.querySelector('input[name=year]').getAttribute('max');
-			// const valMonth = this.newLimit.month;
-			// const minMonth = document.querySelector('input[name=month]').getAttribute('min');
-			// const maxMonth = document.querySelector('input[name=month]').getAttribute('max');
-			// const valDay = this.newLimit.date;
-			// const minDay = document.querySelector('input[name=day]').getAttribute('min');
-			// const maxDay = document.querySelector('input[name=day').getAttribute('max');
-
-			// if(parseInt(valYear, 10) < minYear || parseInt(valYear, 10) > maxYear || parseInt(valMonth, 10) < minMonth || parseInt(valMonth, 10) > maxMonth || parseInt(valDay, 10) < minDay || parseInt(valDay, 10) > maxDay || this.newTodo == '' || this.newLimit.year == '' || this.newLimit.month == '' || this.newLimit.day == '') return;
-
 			if(this.newTodo == '' || this.newLimit.year == '' || this.newLimit.month == '' || this.newLimit.day == '') return;
 
-			const limits = new Date(this.newLimit.year, this.newLimit.month, this.newLimit.date);
+			const limits = new Date(this.newLimit.year, this.newLimit.month -1, this.newLimit.date).toLocaleDateString();
 
 			this.todos.push({
 				id: this.id,
@@ -74,7 +42,12 @@ const vm = new Vue({
 				radioYet: this.radioYet + this.id.toString(),
 				radioProgress: this.radioProgress + this.id.toString(),
 				radioDone: this.radioDone + this.id.toString(),
-				limit: `${limits.getFullYear()}年${limits.getMonth()}月${limits.getDate()}日`,
+				limit: {
+					year: this.newLimit.year,
+					month: this.newLimit.month,
+					date: this.newLimit.date,
+				},
+				limits: limits,
 				status: this.status,
 				isDone: this.isDone
 			});
@@ -83,27 +56,32 @@ const vm = new Vue({
 			this.newLimit.year = '';
 			this.newLimit.month = '';
 			this.newLimit.date = '';
-
-			// this.newLimit.year = new Date().getFullYear();
-			// this.newLimit.month = new Date().getMonth() + 1;
-			// this.newLimit.date = new Date().getDate();
 		},
 
-		// editShow: function(index) {
-		// 	this.isEdit = true;
-		// 	this.editIndex = index;
-		// 	this.editId = this.todos[index].
-		// 	this.editLimit.year = '';
-		// 	this.editLimit.month = '';
-		// 	this.editLimit.date = '';
-		// },
+		editShow: function(index) {
+			this.editIndex = index;
+			this.editTodo = this.todos[index].title;
+			this.editLimit.year = this.todos[index].limit.year;
+			this.editLimit.month = this.todos[index].limit.month;
+			this.editLimit.date = this.todos[index].limit.date;
+			this.isEdit = true;
+		},
 
-		// changeTodo: function(index) {
-		// 	this.isEdit = false;
-		// },
+		changeTodo: function(index) {
+			const editLimits = new Date(this.editLimit.year, this.editLimit.month -1, this.editLimit.date).toLocaleDateString();
+
+			this.todos[index].title = this.editTodo;
+			this.todos[index].limit.year = this.editLimit.year;
+			this.todos[index].limit.month = this.editLimit.month;
+			this.todos[index].limit.date = this.editLimit.date;
+			this.todos[index].limits = editLimits,
+
+			this.isEdit = false;
+		},
 
 		deleteTodo: function(index) {
 			this.todos.splice(index, 1);
+			this.isEdit = false;
 		},
 
 		radioChange: function(index) { // 完了かどうかの判断用
@@ -114,12 +92,4 @@ const vm = new Vue({
 			}
 		}
 	},
-
-	// created: function() { // 現在の年の取得用
-	// 	this.newLimit.year = new Date().getFullYear();
-	// 	this.newLimit.month = new Date().getMonth() + 1;
-	// 	this.newLimit.date = new Date().getDate();
-	// 	this.nowYear  = new Date().getFullYear();
-	// 	this.maxYear = this.nowYear + 10;
-	// },
 });
